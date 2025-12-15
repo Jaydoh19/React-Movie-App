7874bc96af5a7c696b8392420e17ff992632b50e
JaydenBlunt <jjaydog7@gmail.com>
2025-12-14 23:34:38 -0800

    import styling and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css'
-import MovieCard from './components/MovieCard'
+import './css/App.css'
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import Favorites from './pages/Favorites';
+import NavBar from './components/NavBar';
 
 function App() {
-
+  ;
 
   return (
-    <>
-      <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>
-    </>
-  )
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
+
+  );
 }
 
 
